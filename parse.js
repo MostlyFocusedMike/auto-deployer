67846083db8f14c612fed463c4e65e377a5885fa
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2022-02-17 16:25:40 -0500

    refactor to use more regex

--- parse.js
+++ parse.js
@@ -1,44 +1,45 @@
 const { exec } = require("child_process");
 
-const getLogsForThisRelease = (logs, prevRelease = '') => {
+const getLogsForLatestRelease = (logs) => {
   const logsArr = logs.split('\n');
-  const prevTag = `tag: ${prevRelease}`;
-  const prevReleaseIdx = logsArr.findIndex(log => log.search(prevTag) > 0);
+  const releaseMsgStartingPattern = /\) RELEASE \d/;
+  const isLogFromRelease = (log) => log.search(releaseMsgStartingPattern) > 0
+  const prevReleaseIdx = logsArr.findIndex(isLogFromRelease);
   return logsArr.slice(0, prevReleaseIdx).join('\n');
 }
 
-const addProperTicket = (logs) => {
-  const finalLogs = [];
-  logs.split('\n').forEach(log => {
-    const finalLog = log.trim().replace(/^\d{4}/, (num) => `[MKTG-${num}]`);
-    if (finalLog === "Merge branch 'master' into develop") return;
-    finalLogs.push(finalLog);
-  });
-  return finalLogs.join('\n');
+const editLogEntries = (logs) => {
+  // git hash, branch and tag metadata ie [ada19d1 (tag: 1.0.0) ]
+  const gitMetaData = /^[\w]{7}\s(?:\(.*\)\s)?/gm;
+  let formattedLogs = logs.replace(gitMetaData, '');
+
+  const improperTicketFormat = /^\s?\d{4}/gm;
+  formattedLogs = formattedLogs.replace(improperTicketFormat, (num) => `[MKTG-${num}]`);
+
+  formattedLogs = formattedLogs.replace(/\nMerge branch 'master' into develop/g, '');
+  return formattedLogs;
 }
 
 const formatLogs = (logs) => {
-  console.log(logs);
-  console.log('----------------------------:', );
-  const currentReleaseLogs = getLogsForThisRelease(logs, '1.1.0');
-  const regex = /^[\w]{7}\s(?:\(.*\)\s)?/gm;
-  const finalLogs = currentReleaseLogs.replace(regex, '');
-  console.log(addProperTicket(finalLogs));
-  return addProperTicket(finalLogs);
+  const latestReleaseLogs = getLogsForLatestRelease(logs);
+  return editLogEntries(latestReleaseLogs);
 }
 
-// exec("git log --oneline --decorate", (error, stdout, stderr) => {
-//     if (error) {
-//         console.log(`error: ${error.message}`);
-//         return;
-//     }
-//     if (stderr) {
-//         console.log(stderr);
-//         return;
-//     }
-//     formatLogs(stdout)
-// });
+const run = () => {
+  exec("git log --oneline --decorate", (error, stdout, stderr) => {
+      if (error) {
+          console.log(`error: ${error.message}`);
+          return;
+      }
+      if (stderr) {
+          console.log(stderr);
+          return;
+      }
+      console.log(formatLogs(stdout));
+  });
+}
 
 module.exports = {
-  formatLogs
+  formatLogs,
+  run,
 }
