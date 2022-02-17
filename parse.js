bc528b275f7270cdc799a075b014798dd2236846
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2022-02-17 11:39:44 -0500

    [MKTG-6273] get basic script working

--- parse.js
+++ parse.js
@@ -1,13 +1,20 @@
 const { exec } = require("child_process");
 
 const getLogsForThisRelease = (logs, previousVersion = '') => {
-  return logs;
+  const logsArr = logs.split('\n');
+  const upToIndex = logsArr.findIndex(log => log.search(`tag: ${previousVersion}`) > 0)
+  const releaseLogs = logsArr.slice(0, upToIndex)
+  console.log('logsArr:', );
+  return releaseLogs.join('\n');
 }
 
 const formatLogs = (logs) => {
+  console.log('RAW LOGS:');
   console.log(logs);
-  const currentReleaseLogs = getLogsForThisRelease(logs, '1.0.0');
+  console.log('----------------------------:', );
+  const currentReleaseLogs = getLogsForThisRelease(logs, '1.1.0');
   const finalLogs = currentReleaseLogs.replace(/^[\w]{7}\s(?:\(.*\)\s)?/gm, '')
+  console.log('RAW LOGS:');
   console.log(finalLogs);
 }
 
